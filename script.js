05c8d439ca27d048af89d93c811b36d02146a056
Fenil <fenil3357@gmail.com>
2021-10-21 00:12:29 -0700

    songs added

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("This is not actual spotify clone. This is just for demo purpose")
+
+let songIdx = 0;
+let audioElement = new Audio('song1.mp3')
+
+let songs = [
+    {songName: "New Rules", filepath:"song1.mp3"}
+]
+
+let audioElement = new Audio('song1.mp3')
+
